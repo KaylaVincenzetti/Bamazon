d3737cd186c0587b5614a287d56f89053579164b
Kayla V <knvg14@gmail.com>
2018-05-30 23:23:08 -0600

    display items

---
+++ bamazonCustomer.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "root",
+    database: "bamazon_db"
+
+
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    
+});
+
+function displayProducts() {
+    console.log("Displaying all products...\n");
+    connection.query("select * from products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
+
+displayProducts();
